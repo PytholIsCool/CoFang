d4093caefd951fd7e8740c6d028b5378963833bc
Pythol <95198306+PytholIsCool@users.noreply.github.com>
2025-02-03 12:17:31 -0500

    Delete PyConfig directory

--- PyConfig/PyConfig/API_Includes.h
+++
@@ -1,9 +0,0 @@
-#ifndef API_INCLUDES_H
-#define API_INCLUDES_H
-
-#include "PyConfig.Base/PyConfig.Base.Objects/ConfigObject.h"
-#include "PyConfig.Base/PyConfig.Base.FileSystemWrapper/File.h"
-#include "PyConfig.Base/PyConfig.Base.FileSystemWrapper/Path.h"
-#include "PyConfig.Base/Serialization.h"
-
-#endif
